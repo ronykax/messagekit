9d747ee2a5f9ce497ad7d5765b79f1d27c5616d5
Rony Kati <73883742+ronykax@users.noreply.github.com>
2025-09-30 01:16:59 +0530

    redirect to /select-guild upon login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+"use client";
+
 import { ExternalLinkIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { useUserStore } from "@/lib/stores/user";
 
 export default function Page() {
+    const { user } = useUserStore();
+
+    if (user) {
+        window.location.href = "/select-guild";
+    }
+
     return (
         <div className="flex items-center justify-center h-screen">
             <Button variant={"link"} className="text-white" asChild>
-                <a href="/auth/login">
+                <a href="/auth/login?redirect=/select-guild">
                     Login to use Message Kit
                     <ExternalLinkIcon />
                 </a>
