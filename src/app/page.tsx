70fd1a29abdb261058c2671954fb5c23b163aff6
Rony Kati <73883742+ronykax@users.noreply.github.com>
2025-09-30 01:13:54 +0530

    login button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import { ExternalLinkIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
-    return "hi world!";
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <Button variant={"link"} className="text-white" asChild>
+                <a href="/auth/login">
+                    Login to use Message Kit
+                    <ExternalLinkIcon />
+                </a>
+            </Button>
+        </div>
+    );
 }
